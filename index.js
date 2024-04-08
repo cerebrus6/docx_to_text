083efc19cd6c90ea55b6a4fa0ae4612d036d5ae5
cerebrus6 <diokristofferyap.dky@gmail.com>
2024-04-08 13:32:46 +0800

    enhc: add option to specify directory to be used

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 const mammoth = require('mammoth');
 
 // Function to convert DOCX files to TXT
@@ -25,13 +26,55 @@ async function convertDocxToTxt(docxFilePath) {
     }
 }
 
-// Get the current directory
-const currentDirectory = process.cwd();
+// Ask for directory path
+var directory = process.argv[2] ?? false;
 
-// Loop through all files in the current directory
-fs.readdirSync(currentDirectory).forEach(filename => {
-    if (filename.endsWith('.docx')) {
-        const docxFilePath = path.join(currentDirectory, filename);
-        convertDocxToTxt(docxFilePath);
-    }
-});
+if(!directory) {
+	// Create interface to read input from command line
+	const rl = readline.createInterface({
+	    input: process.stdin,
+	    output: process.stdout
+	});
+
+	rl.question("Enter the directory path: ", function(targetDirectory) {
+	    // Replace backslashes with forward slashes
+	    targetDirectory = targetDirectory.replace(/"/g, '')
+
+	    // Check if directory exists
+	    if (!fs.existsSync(targetDirectory) && targetDirectory !== '') {
+	        console.error("Directory does not exist.");
+	        rl.close();
+	        return;
+	    }
+
+		convert_all_docx_in_directory(targetDirectory)
+
+	    // Close the interface
+	    rl.close();
+	});
+
+} else {
+    // Replace backslashes with forward slashes
+    let targetDirectory = directory.replace(/"/g, '')
+	convert_all_docx_in_directory(targetDirectory);
+}
+
+
+async function convert_all_docx_in_directory(targetDirectory = "") {
+	    // Replace backslashes with forward slashes
+	    targetDirectory = targetDirectory.replace(/"/g, '')
+
+	    // Default directory is current directory
+	    if (targetDirectory === '') {
+	        targetDirectory = process.cwd();
+	    	console.log('No directory given, using current directory: ' + targetDirectory);
+	    }
+
+	    // Loop through all files in the target directory
+	    fs.readdirSync(targetDirectory).forEach(filename => {
+	        if (filename.endsWith('.docx')) {
+	            const docxFilePath = path.join(targetDirectory, filename);
+	            convertDocxToTxt(docxFilePath);
+	        }
+	    });
+}
